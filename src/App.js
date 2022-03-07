795c1df10e43d9aacf408137615bac98ccd1e5e1
Ajna Moreira <ajna.moreira@gmail.com>
2022-03-07 16:25:44 -0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import { Fragment } from 'react'
 import CountdownTimer from './Components/CountdownTimer'
-import { Container } from '@mui/material'
 
 function App() {
     return (
         <Fragment>
-            <Container component="main">
-                <CountdownTimer />
-            </Container>
+            <CountdownTimer />
         </Fragment>
     )
 }
